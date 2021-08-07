748f29ded929e50cbc1b650a098f1d3595b0444c
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-08-07 13:17:07 -0500

    initial Commit

---
+++ public/images/hello.md
@@ -0,0 +1,3 @@
+# Images Use
+
+Mainly used for having Picture to align with the kana or word.
---
+++ server/server.js
@@ -0,0 +1,8 @@
+let express = require("express");
+let app = express();
+let cors = require("cors");
+app.use(cors());
+app.use(express.json());
+let port = 4500;
+
+app.listen(port, () => console.log(`Server up and running on ${port}...`));
