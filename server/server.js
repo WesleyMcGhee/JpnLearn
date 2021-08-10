d8742c2da64c71f5bb149053c1adbe2c3f3bd701
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-08-10 15:36:48 -0500

    worked on backend

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let app = express();
 let cors = require("cors");
+const ctrl = require("./controller/controller.js");
 app.use(cors());
 app.use(express.json());
 let port = 4500;
